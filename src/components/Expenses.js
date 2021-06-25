606138f9660fb76d56419a0bdb1a0ca0f85aa5a1
shrivastava-prateek <prateek.shrivastava@noreply.github.com>
2021-06-25 10:03:26 +0530

    concise

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,28 +4,14 @@ import './Expenses.css';
 function Expenses(props) {
   const expenses = props.items;
 
+  const expenseList = expenses.map( expense =>  <ExpenseItem title= {expense.title} 
+    amount={expense.amount} 
+    date={expense.date}/>
+    );
+
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      {expenseList}
     </div>
   );
 }
